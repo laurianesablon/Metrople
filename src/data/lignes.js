2e86180817022626aaba867e3c3045040270ef82
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-10-01 21:24:54 +0200

    fonction lignes

--- src/data/lignes.js
+++ src/data/lignes.js
@@ -1,22 +1,46 @@
-export const lignesWithColors = [
-  { Ligne: "Ligne 1", color: "#FDCE02" },
-  { Ligne: "Ligne 2", color: "#1763B0" },
-  { Ligne: "Ligne 3", color: "#9F9824" },
-  { Ligne: "Ligne 3bis", color: "#C14191" },
-  { Ligne: "Ligne 4", color: "#F28E42" },
-  { Ligne: "Ligne 5", color: "#82C391" },
-  { Ligne: "Ligne 6", color: "#F4A4BB" },
-  { Ligne: "Ligne 7", color: "#82C391" },
-  { Ligne: "Ligne 7bis", color: "#CFACD3" },
-  { Ligne: "Ligne 8", color: "#D5C902" },
-  { Ligne: "Ligne 9", color: "#E3B429" },
-  { Ligne: "Ligne 10", color: "#8D5E2B" },
-  { Ligne: "Ligne 11", color: "#06814F" },
-  { Ligne: "Ligne 12", color: "#99D4E2" },
-  { Ligne: "Ligne 13", color: "#652483" },
-  { Ligne: "Ligne 14", color: "#652483" },
+const lignesNum = [
+  "Ligne 1",
+  "Ligne 2",
+  "Ligne 3",
+  "Ligne 3bis",
+  "Ligne 4",
+  "Ligne 5",
+  "Ligne 6",
+  "Ligne 7",
+  "Ligne 7bis",
+  "Ligne 8",
+  "Ligne 9",
+  "Ligne 10",
+  "Ligne 11",
+  "Ligne 12",
+  "Ligne 13",
+  "Ligne 14",
 ];
-function lignes(data) {
-  return data.features.filter((item) => lignesWithColors.Ligne.includes(item.properties.Ligne));
+
+export const lignesWithColors = [
+  { Ligne: "Ligne 1", color: "#FFBE00" },
+  { Ligne: "Ligne 2", color: "#0055C8" },
+  { Ligne: "Ligne 3", color: "#6E6E00" },
+  { Ligne: "Ligne 3bis", color: "#6EC4E8" },
+  { Ligne: "Ligne 4", color: "#A0006E" },
+  { Ligne: "Ligne 5", color: "#FF7E2E" },
+  { Ligne: "Ligne 6", color: "#6ECA97" },
+  { Ligne: "Ligne 7", color: "#FA9ABA" },
+  { Ligne: "Ligne 7bis", color: "#6ECA97" },
+  { Ligne: "Ligne 8", color: "#D282BE" },
+  { Ligne: "Ligne 9", color: "#B6BD00" },
+  { Ligne: "Ligne 10", color: "#C9910D" },
+  { Ligne: "Ligne 11", color: "#704B1C" },
+  { Ligne: "Ligne 12", color: "#06814F" },
+  { Ligne: "Ligne 13", color: "#6EC4E8" },
+  { Ligne: "Ligne 14", color: "#62259D" },
+];
+export default function lignes(data) {
+  const lignesObj = {};
+
+  lignesNum.forEach((ligne) => {
+    lignesObj[`ligne_${ligne.split(' ')[1]}`] = data.features.filter((item) => item.properties.Ligne === ligne);
+  });
+
+  return lignesObj;
 }
-export default lignes;
