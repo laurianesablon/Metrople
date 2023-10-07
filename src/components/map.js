ec61843344e287e50f76a8459fb0afc6a0d6bf1e
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-10-07 10:44:49 +0200

    mise des  différentes fonctions dans un autre fichier

--- src/components/map.js
+++ src/components/map.js
@@ -1,68 +1,32 @@
-import React, { useEffect } from "react";
 import * as d3 from "d3";
 import data from "../data/data.geojson";
-import lignes from "../data/lignes";
 import { lignesWithColors } from '../data/lignes';
 import traces from '../data/paths/metroPaths.geojson'
 import paris from '../data/paths/paris.geojson';
+import { useEffect, useRef } from "react";
+import { renderStationPoints, renderMetroPaths, renderParisPaths } from "./mapElements";
 
 export function Map() {
-  const width = 700;
-  const height = 650;
+  const width = 740;
+  const height = 600;
   const projection = d3.geoEqualEarth();
-  let svg = d3.select("body").select("svg");
+  const svgRef = useRef(null);
 
-  if (svg.empty()) {
-    svg = d3.select("body").append("svg")
-      .attr("class", "map")
-      .style("width", width)
-      .style("height", height);
-  }
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+  
+    const fetchData = async () => {
+      const station = await d3.json(data);
+      const tracesData = await d3.json(traces);
+      const parisPerimeter = await d3.json(paris);
+  
+      renderStationPoints(station, svg, height, width);
+      renderMetroPaths(tracesData, svg, height, width);
+      renderParisPaths(parisPerimeter, svg, height, width);
+    };
+  
+    fetchData();
+  }, []);
 
-  d3.json(data).then(function(station) {
-    projection.fitSize([width, height], station);
-    const geoGenerator = d3.geoPath().projection(projection);
-
-    const paths = svg.selectAll('path')
-    .data(station.features)
-    .join('circle') // Change 'path' to 'circle' to render station points as circles
-    .attr('cx', d => projection(d.geometry.coordinates)[0])
-    .attr('cy', d => projection(d.geometry.coordinates)[1])
-    .attr('r', 2.5) // Adjust the desired size of the station points
-    .attr('class', 'station-point') 
-    .attr('fill', (d) => {
-      const color = lignesWithColors.find(ligne => ligne.Ligne === d.properties.Ligne);
-      return color ? color.color : 'black';
-    });
-
-    d3.json(traces).then(function(tracesData) {
-      const metroPaths = svg.selectAll('.metro-path')
-        .data(tracesData.features)
-        .join('path')
-        .attr('class', 'metro-path')
-        .attr('d', geoGenerator)
-        .attr('stroke', (d) => {
-            const color = lignesWithColors.find(ligne => ligne.Ligne === d.properties.Ligne);
-            return color ? color.color : 'black';
-          })
-        .attr('stroke-width', 2)
-        .attr('fill', 'none');
-    });
-    d3.json(paris).then(function(parisData) {
-        const parisPaths = svg.selectAll('.paris-path')
-          .data([parisData])
-          .join('path')
-          .attr('class', 'paris-path')
-          .attr('d', geoGenerator)
-          .attr('stroke', 'grey')
-          .attr('stroke-width', 1)
-          .attr('fill', 'none');
-      });
-
-
-  });
-
-  return (
-    <p>map</p>
-  );
+  return <svg ref={svgRef} width={width} height={height} />;
 }
---
+++ src/components/mapElements.js
@@ -0,0 +1,54 @@
+import * as d3 from "d3";
+import { lignesWithColors } from '../data/lignes';
+
+export const renderStationPoints = (station, svg, height, width) => {
+    const projection = d3.geoMercator().fitSize([width, height], station);
+    const geoGenerator = d3.geoPath().projection(projection);
+    const paths = svg
+      .selectAll("circle")
+      .data(station.features)
+      .join("circle")
+      .attr("cx", (d) => projection(d.geometry.coordinates)[0])
+      .attr("cy", (d) => projection(d.geometry.coordinates)[1])
+      .attr("r", 2.5)
+      .attr("class", "station-point")
+      .attr("fill", (d) => {
+        const color = lignesWithColors.find(
+          (ligne) => ligne.Ligne === d.properties.Ligne
+        );
+        return color ? color.color : "black";
+      });
+  };
+
+  export const renderMetroPaths = (tracesData, svg, height, width) => {
+    const projection = d3.geoMercator().fitSize([width, height], tracesData);
+    const geoGenerator = d3.geoPath().projection(projection);
+    const metroPaths = svg
+      .selectAll(".metro-path")
+      .data(tracesData.features)
+      .join("path")
+      .attr("class", "metro-path")
+      .attr("d", geoGenerator)
+      .attr("stroke", (d) => {
+        const color = lignesWithColors.find(
+          (ligne) => ligne.Ligne === d.properties.Ligne
+        );
+        return color ? color.color : "black";
+      })
+      .attr("stroke-width", 2)
+      .attr("fill", "none");
+  };
+
+  export const renderParisPaths = (parisPerimeter, svg, height, width) => {
+    const projection = d3.geoMercator().fitSize([width, height], parisPerimeter);
+    const geoGenerator = d3.geoPath().projection(projection);
+    const parisPaths = svg
+      .selectAll(".paris-path")
+      .data([parisPerimeter])
+      .join("path")
+      .attr("class", "paris-path")
+      .attr("d", geoGenerator)
+      .attr("stroke", "grey")
+      .attr("stroke-width", 1)
+      .attr("fill", "none");
+  };
