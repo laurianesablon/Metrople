6ba975c24ed59c3b0a15696b21eb6606aeca6872
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-10-07 10:46:44 +0200

    ajouts css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style/dist/main.css';
+import './style/dist/header.css';
+import './style/dist/map.css';
+import './style/dist/app.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
