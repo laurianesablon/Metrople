0fe79d746e3e458ab5106ffc47175253fe4be8ad
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-10-18 16:57:39 +0200

    muse du composent map dans home

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout";
+import Map from "../components/map";
+
+export default function Home() {
+  return (
+    <>
+      <Layout />
+      <main>
+        <Map />
+      </main>
+    </>
+  );
+}
