c6407fab1d8c02683b8bf91f5949e237e0c8b7f3
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-10-18 18:19:47 +0200

    ajout loader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import SingleLinePage from "./pages/singleLinePage";
-import { Data } from "./components/data";
 import { fetchData } from "./store/store";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 function App() {
   const dispatch = useDispatch();
-
+  const loader = useSelector((state) => state.data.loading);
+  console.log(loader);
   useEffect(() => {
-    dispatch(fetchData())
-   }, []);
+    dispatch(fetchData());
+  }, []);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="ligne/:ligne" element={<SingleLinePage />} />
-        </Routes>
-      </BrowserRouter>
+      {loader !== "fulfilled" && (
+        <div className="loader-container">
+          <span className="loader"></span>
+        </div>
+      )}
+      {loader === "fulfilled" && (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="ligne/:ligne" element={<SingleLinePage />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   );
 }
