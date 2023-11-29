d88304d37d12a266d1b94abf4899f5d99df07589
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-11-29 17:15:35 +0100

    added form, it's almost completly functionnal. i will need to add the reset of the input when the user finds a station, and also changed the color of the station accrding to it's metro line's color

--- src/components/map.js
+++ src/components/map.js
@@ -1,22 +1,56 @@
 import * as d3 from "d3";
-
-import { useRef } from "react";
-import { renderStationPoints, renderMetroPaths, renderParisPerimeter } from "./mapElements";
+import { useEffect, useRef, useState } from "react";
+import {
+  renderAllStationPoints,
+  renderAllMetroPaths,
+  renderParisPerimeter,
+  findStation,
+} from "./mapElements";
 import { useDispatch, useSelector } from "react-redux";
+import { render } from "@testing-library/react";
 
 export default function Map() {
-  const width = 740;
-  const height = 600;
   const svgRef = useRef(null);
   const dispatch = useDispatch();
-  const svg = d3.select(svgRef.current);
+  const [stationInput, setStationInput] = useState('');
 
-  const metroStations = useSelector((state) => state.data.metroStations);
-  const metroLines = useSelector((state) => state.data.metroLines);
-  const parisPerimeter = useSelector((state) => state.data.parisPerimeter);
+  const onInputChange = (e) => {
+    e.preventDefault();
+    setStationInput(e.target.value);
+  };
+  const { metroStations, metroLines, parisPerimeter } =
+    useSelector(state => state.data);
 
-    renderStationPoints(metroStations, svg, height, width);
-    renderMetroPaths(metroLines, svg);
-    renderParisPerimeter(parisPerimeter, svg);
-  return <svg ref={svgRef} width={width} height={height} />;
+  const drawMap = () => {
+    const svg = d3.select(svgRef.current);
+    const { width, height } = { width: 740, height: 600 };
+    renderAllStationPoints(metroStations, svg, height, width);
+    renderParisPerimeter(
+      parisPerimeter, svg, height, width, metroStations
+    );
+  };
+
+  useEffect(drawMap, [metroStations, parisPerimeter]);
+
+  useEffect(() => {
+    if (stationInput) {
+      const station = document.getElementById(stationInput);
+      if (station) station.style.fill = "red";
+    }
+  }, [stationInput]);
+
+  return (
+    <>
+      <form onSubmit={e => e.preventDefault()}>
+        <input
+          type="text"
+          value={stationInput}
+          onChange={onInputChange}
+          placeholder="Enter station"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <svg ref={svgRef} width="740" height="600" />
+    </>
+  );
 }
