284d963d6e354068ea86fa80cb0373e5521d23b2
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-11-29 17:16:58 +0100

    added a find station function and a function to color stations (not yet functionnal)

--- src/components/mapElements.js
+++ src/components/mapElements.js
@@ -1,51 +1,77 @@
 import * as d3 from "d3";
-import { lignesWithColors } from '../data/lines';
+import { linesWithColors } from "../data/lines";
 
-let geoGenerator = undefined
-export const renderStationPoints = (station, svg, height, width) => {
-    const projection = d3.geoMercator().fitSize([width, height], station);
-    geoGenerator = d3.geoPath().projection(projection);
-    const paths = svg
-      .selectAll("circle")
-      .data(station.features)
-      .join("circle")
-      .attr("cx", (d) => projection(d.geometry.coordinates)[0])
-      .attr("cy", (d) => projection(d.geometry.coordinates)[1])
-      .attr("r", 2.5)
-      .attr("class", "station-point")
-      .attr("fill", (d) => {
-        const color = lignesWithColors.find(
-          (ligne) => ligne.Ligne === d.properties.Ligne
-        );
-        return color ? color.color : "black";
-      });
-  };
+let geoGenerator = undefined;
+export const renderAllStationPoints = (station, svg, height, width) => {
+  const projection = d3.geoMercator().fitSize([width, height], station);
+  geoGenerator = d3.geoPath().projection(projection);
+  const paths = svg
+    .selectAll("circle")
+    .data(station.features)
+    .join("circle")
+    .attr("cx", (d) => projection(d.geometry.coordinates)[0])
+    .attr("cy", (d) => projection(d.geometry.coordinates)[1])
+    .attr("r", 2.5)
+    .attr("class", "station-point")
+    .attr("id", d => `${d.properties.stop_name}`)
+    .attr("fill", "transparent");
+    // .attr("fill", (d) => {
+    //   const color = linesWithColors.find(
+    //     (ligne) => ligne.Ligne === d.properties.Ligne
+    //   );
+    //   return color ? color.color : "black";
+    // });
+};
 
-  export const renderMetroPaths = (tracesData, svg) => {
-    const metroPaths = svg
-      .selectAll(".metro-path")
-      .data(tracesData.features)
-      .join("path")
-      .attr("class", "metro-path")
-      .attr("d", geoGenerator)
-      .attr("stroke", (d) => {
-        const color = lignesWithColors.find(
-          (ligne) => ligne.Ligne === d.properties.Ligne
-        );
-        return color ? color.color : "black";
-      })
-      .attr("stroke-width", 2)
-      .attr("fill", "none");
-  };
+export const colorChosenStation = (svgCanvas, stationName) => {
+  svgCanvas.selectAll('.station-point')
+    .filter(d => d.properties.Name === stationName)
+    .attr('fill', d => {
+      const lineInfo = linesWithColors.find(
+        line => line.Ligne === d.properties.Line
+      );
+      return lineInfo ? lineInfo.color : 'white';
+    });
+};
+export const renderAllMetroPaths = (tracesData, svg) => {
+  const metroPaths = svg
+    .selectAll(".metro-path")
+    .data(tracesData.features)
+    .join("path")
+    .attr("class", "metro-path")
+    .attr("d", geoGenerator)
+    .attr("stroke", (d) => {
+      const color = linesWithColors.find(
+        (ligne) => ligne.Ligne === d.properties.Ligne
+      );
+      return color ? color.color : "black";
+    })
+    .attr("stroke-width", 2)
+    .attr("fill", "none");
+};
 
-  export const renderParisPerimeter = (parisPerimeter, svg) => {
-    const parisPaths = svg
-      .selectAll(".paris-path")
-      .data([parisPerimeter])
-      .join("path")
-      .attr("class", "paris-path")
-      .attr("d", geoGenerator)
-      .attr("stroke", "grey")
-      .attr("stroke-width", 1)
-      .attr("fill", "none");
-  };
+export const renderParisPerimeter = (parisPerimeter, svg, height, width, station) => {
+  const projection = d3.geoMercator().fitSize([width, height], station);
+  geoGenerator = d3.geoPath().projection(projection);
+  const parisPaths = svg
+    .selectAll(".paris-path")
+    .data([parisPerimeter])
+    .join("path")
+    .attr("class", "paris-path")
+    .attr("d", geoGenerator)
+    .attr("stroke", "grey")
+    .attr("stroke-width", 1)
+    .attr("fill", "none");
+};
+
+export function findStation(list, stationName) {
+  for (let i = 0; i < list.length; i++) {
+    let item = list[i];
+    if (item.properties.stop_name.includes(stationName)) {
+      // console.log(item);
+      return item;
+    }
+  }
+
+  return null;
+}
