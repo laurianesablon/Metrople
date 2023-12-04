35b16a3c10b9370488629ea1647bead94bbd7c51
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-12-04 16:02:40 +0100

    added form here

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,32 @@
 import Layout from "../components/layout";
 import Map from "../components/map";
+import { useState } from "react";
 
 export default function Home() {
+  const [stationInput, setStationInput] = useState("");
+  const [stationCount, setStationsCount] = useState(0);
+
+
+  const onInputChange = (e) => {
+    e.preventDefault();
+    setStationInput(e.target.value);
+  };
   return (
     <>
       <Layout />
-      <main>
-        <div className="map_container">
-          <Map />
+      <main className="flex flex-col justify-center items-center bg-black">
+        <div className="w-full">
+          <form onSubmit={(e) => e.preventDefault()}>
+            <input
+              type="text"
+              value={stationInput}
+              onChange={onInputChange}
+              placeholder="Enter station"
+              className="font-mono"
+            />
+          </form>
+          <p>{stationCount}/308</p>
+          <Map stationInput={stationInput} setStationsCount={setStationsCount} />
         </div>
       </main>
     </>
