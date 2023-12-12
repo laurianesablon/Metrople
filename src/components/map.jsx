77912e90c029a7103296a1f7b0f16241a3bf3d05
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-12-12 13:21:54 +0100

    added function that finds the stations and renders them, although there is a re-rendering problem now that the whole map rerenders anytime user finds a station

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,14 +1,18 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import { MAPBOX_ACCESS_TOKEN } from "../utils/config.jsx";
-import { renderAllMetroPaths } from "../utils/mapElements.jsx";
-import { renderParisPerimeter } from "../utils/mapElements.jsx";
-import { setStationColor } from "../utils/utils.jsx";
 import metroLinesData from "../data/metroLines.json";
 import metroStationsData from "../data/metroStations.json";
 
+const emptyGeoJSONTemplate = {
+  type: "FeatureCollection",
+  name: "jsontemplate",
+  features: [],
+};
+
 export default function ParisMap({ stationInput, setStationsCount }) {
-  const [foundStations, setFoundStations] = useState([]);
+  const [foundStations, setFoundStations] = useState(emptyGeoJSONTemplate);
+
   useEffect(() => {
     mapboxgl.accessToken = MAPBOX_ACCESS_TOKEN;
     const map = new mapboxgl.Map({
@@ -18,86 +22,44 @@ export default function ParisMap({ stationInput, setStationsCount }) {
       zoom: 11,
       pitch: 40,
     });
+
     map.on("style.load", () => {
       const layers = map.getStyle().layers;
       const labelLayerId = layers.find(
         (layer) => layer.type === "symbol" && layer.layout["text-field"]
       ).id;
 
-      map.addLayer(
-        {
-          id: "add-3d-buildings",
-          source: "composite",
-          "source-layer": "building",
-          filter: ["==", "extrude", "true"],
-          type: "fill-extrusion",
-          minzoom: 15,
-          paint: {
-            "fill-extrusion-color": "#aaa",
-            "fill-extrusion-height": [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              15,
-              0,
-              15.05,
-              ["get", "height"],
-            ],
-            "fill-extrusion-base": [
-              "interpolate",
-              ["linear"],
-              ["zoom"],
-              15,
-              0,
-              15.05,
-              ["get", "min_height"],
-            ],
-            "fill-extrusion-opacity": 0.6,
-          },
+      map.addLayer({
+        id: "add-3d-buildings",
+        source: "composite",
+        "source-layer": "building",
+        filter: ["==", "extrude", "true"],
+        type: "fill-extrusion",
+        minzoom: 15,
+        paint: {
+          "fill-extrusion-color": "#aaa",
+          "fill-extrusion-height": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            15,
+            0,
+            15.05,
+            ["get", "height"],
+          ],
+          "fill-extrusion-base": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            15,
+            0,
+            15.05,
+            ["get", "min_height"],
+          ],
+          "fill-extrusion-opacity": 0.6,
         },
-        labelLayerId
-      );
+      }, labelLayerId);
 
-      // const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-      //   station.properties.stop_name
-      // );
-      const popup = new mapboxgl.Popup({
-        closeButton: false,
-        closeOnClick: false,
-      });
-
-      map.on("mouseenter", "metro-stations", (e) => {
-        // Change the cursor style as a UI indicator.
-        map.getCanvas().style.cursor = "pointer";
-
-        // Copy coordinates array.
-        const coordinates = e.features[0].geometry.coordinates.slice();
-        const stationName = e.features[0].properties.stop_name;
-
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-          coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-        }
-
-        // Populate the popup and set its coordinates
-        // based on the feature found.
-        popup.setLngLat(coordinates).setHTML(stationName).addTo(map);
-      });
-
-      map.on("mouseleave", "metro-stations", () => {
-        map.getCanvas().style.cursor = "";
-        popup.remove();
-      });
-
-      //   new mapboxgl.Marker()
-      //     .setLngLat(station.geometry.coordinates)
-      //     .setPopup(popup)
-      //     .addTo(map);
-      // });
-
-      // Render metro lines
       map.addSource("metro-lines", {
         type: "geojson",
         data: metroLinesData,
@@ -105,22 +67,58 @@ export default function ParisMap({ stationInput, setStationsCount }) {
 
       map.addSource("metro-stations", {
         type: "geojson",
-        data: metroStationsData,
+        data: foundStations,
       });
-      map.addLayer(
-        {
-          id: "metro-stations",
-          type: "circle",
-          source: "metro-stations",
-          paint: {
-            "circle-color": ["get", "route_color"],
-            "circle-radius": 3,
-          },
+
+      map.addLayer({
+        id: "metro-stations",
+        type: "circle",
+        source: "metro-stations",
+        paint: {
+          "circle-color": ["get", "route_color"],
+          "circle-radius": 3,
         },
-        labelLayerId
-      );
+      }, labelLayerId);
     });
-  }, []);
+
+    const popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false,
+    });
+
+    map.on("mouseenter", "metro-stations", (e) => {
+      map.getCanvas().style.cursor = "pointer";
+
+      const coordinates = e.features[0].geometry.coordinates.slice();
+      const stationName = e.features[0].properties.stop_name;
+
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      popup.setLngLat(coordinates).setHTML(stationName).addTo(map);
+    });
+
+    map.on("mouseleave", "metro-stations", () => {
+      map.getCanvas().style.cursor = "";
+      popup.remove();
+    });
+  }, [foundStations]);
+
+  useEffect(() => {
+    const matchingStations = metroStationsData.features.filter(
+      (station) => station.properties.stop_name.toLowerCase() === stationInput.toLowerCase()
+    );
+
+    if (matchingStations.length > 0) {
+      setFoundStations({
+        type: "FeatureCollection",
+        name: "jsontemplate",
+        features: [...foundStations.features, ...matchingStations],
+      });
+      setStationsCount(foundStations.features.length + matchingStations.length);
+    }
+  }, [stationInput]);
 
   return <div id="map" style={{ width: "800px", height: "600px" }}></div>;
 }
