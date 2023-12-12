09ca5eb70c8379681a6edacf27f88e42b3892309
Lauriane <109591568+stressedoutdyk3@users.noreply.github.com>
2023-12-12 13:22:12 +0100

    added 3dBuildings function

--- src/utils/mapElements.jsx
+++ src/utils/mapElements.jsx
@@ -70,3 +70,32 @@ export function findStation(list, stationName) {
 
   return null;
 }
+
+export function add3DBuildings(map) {
+  const layers = map.getStyle().layers;
+  const labelLayerIdx = layers.findIndex(
+    (layer) => layer.type === "symbol" && layer.layout["text-field"]
+  );
+  const labelLayerId = layers[labelLayerIdx].id;
+
+  map.addLayer({
+    id: "3d-buildings",
+    source: "composite",
+    "source-layer": "building",
+    filter: ["==", "extrude", "true"],
+    type: "fill-extrusion",
+    minzoom: 15,
+    paint: {
+      "fill-extrusion-color": "#aaa",
+      "fill-extrusion-height": [
+        "interpolate", ["linear"], ["zoom"],
+        15, 0, 15.05, ["get", "height"]
+      ],
+      "fill-extrusion-base": [
+        "interpolate", ["linear"], ["zoom"],
+        15, 0, 15.05, ["get", "min_height"]
+      ],
+      "fill-extrusion-opacity": 0.6
+    }
+  }, labelLayerId);
+}
